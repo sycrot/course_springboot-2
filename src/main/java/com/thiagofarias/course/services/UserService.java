2900ffe9ff3f9e28af1f94da7e77dc49f3fb6a08
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-21 09:59:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/thiagofarias/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.thiagofarias.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagofarias.course.entities.User;
+import com.thiagofarias.course.repositories.UserRespository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRespository repository;
+	
+	public List<User> findAll() {
+		
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+
+}
