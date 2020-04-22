3b2565101b94901ecaf234a5017374675ef34b06
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-22 11:19:04 -0300

    Exception handling - delete

--- src/main/java/com/thiagofarias/course/services/UserService.java
+++ src/main/java/com/thiagofarias/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.thiagofarias.course.entities.User;
 import com.thiagofarias.course.repositories.UserRespository;
+import com.thiagofarias.course.services.exceptions.DatabaseException;
 import com.thiagofarias.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -36,7 +39,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
